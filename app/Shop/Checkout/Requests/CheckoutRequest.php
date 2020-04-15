a71b431ffdf22cf63b2db6e78a594bbdeef5c831
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2020-04-15 23:06:07 +0000

    Acerto no carrinho de compras

---
+++ app/Shop/Checkout/Requests/CheckoutRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Shop\Checkout\Requests;
+
+use App\Shop\Base\BaseFormRequest;
+
+/**
+ * Class CartCheckoutRequest
+ * @package App\Shop\Cart\Requests
+ * @codeCoverageIgnore
+ */
+class CheckoutRequest extends BaseFormRequest
+{
+
+
+
+    public function messages()
+    {
+        $messages = [
+            'payment_method.required' => 'Por favor escolha um método de Pagamento',
+        ];
+
+        return $messages;
+
+    }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'payment_method' => ['required']
+        ];
+    }
+}
