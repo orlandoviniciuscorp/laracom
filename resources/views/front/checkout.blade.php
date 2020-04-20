a44a62392969154696f5ce3c4c12c05675bcf77f
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2020-04-20 18:50:35 +0000

    Banco

--- resources/views/front/checkout.blade.php
+++ resources/views/front/checkout.blade.php
@@ -182,10 +182,16 @@
                                                </td>
                                                <td>
                                                    @if($payment['name'] =='Transferência Bancária')
-                                                       <button type="button" class="btn btn-warning"
+                                                       <button type="button" class="btn btn-success"
                                                                data-toggle="modal"
-                                                               data-target="#bank_details">
-                                                           <i class="fa fa-eye"></i>Dados Bancários</button>
+                                                               data-target="#banco_do_brasil">
+                                                           <i class="fa fa-university" aria-hidden="true"></i>
+                                                            Banco do Brasil</button>
+
+                                                       <button type="button" class="btn btn-success"
+                                                               data-toggle="modal"
+                                                               data-target="#nubank">
+                                                           <i class="fa fa-university" aria-hidden="true"></i> Nubank</button>
                                                    @endif
                                                </td>
 
@@ -202,22 +208,39 @@
                             </div>
                         </div>
 
-                        <div class="modal fade" id="bank_details" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                        <div class="modal fade" id="banco_do_brasil" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
                             <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                            <hr />
-                                            <h3>Banco: Banco do Brasil</h3>
-                                            <hr>
-                                            <p>Código do Banco: <strong>001</strong></p>
-                                            <p>Tipo de Conta: <strong>Conta Corrente</strong></p>
-                                            <p>Beneficiário: <strong>Jenifer Soares Medeiros</strong></p>
-                                            <p>Agência: <strong>0315-8</strong></p>
-                                            <p>Número da Conta: <strong> 51095-5</strong></p>
-                                            <p>CPF: <strong>150.557.347-52</strong></p>
-                                            <p>Valor: <strong> {{ config('cart.currency_symbol') }} {{ $total }}</strong></p>
-                                            <p><strong><small class="text-danger text">* {{ config('bank-transfer.note') }}</small></strong></p>
-                                            <p><strong><small class="text-danger text">*Enviar o comprovante de depósito para o  número: (21) 99698-2844 - Sarita Marques</small></strong></p>
-
+                                    <div class="modal-content">
+                                        <hr />
+                                        <h3>Banco: Banco do Brasil</h3>
+                                        <hr>
+                                        <p>Código do Banco: <strong>001</strong></p>
+                                        <p>Tipo de Conta: <strong>Conta Corrente</strong></p>
+                                        <p>Beneficiário: <strong>Jenifer Soares Medeiros</strong></p>
+                                        <p>Agência: <strong>0315-8</strong></p>
+                                        <p>Número da Conta: <strong> 51095-5</strong></p>
+                                        <p>CPF: <strong>150.557.347-52</strong></p>
+                                        <p>Valor: <strong> {{ config('cart.currency_symbol') }} {{ $total }}</strong></p>
+                                        <p><strong><small class="text-danger text">* {{ config('bank-transfer.note') }}</small></strong></p>
+                                        <p><strong><small class="text-danger text">*Enviar o comprovante de depósito para o  número: (21) 99698-2844 - Sarita Marques</small></strong></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal fade" id="nubank" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                            <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <hr />
+                                        <h3>Banco: Nubank</h3>
+                                        <hr>
+                                        <p>Código do Banco: <strong>260</strong></p>
+                                        <p>Tipo de Conta: <strong>Conta Corrente</strong></p>
+                                        <p>Beneficiário: <strong>Sarita de Cássia Coelho Marques</strong></p>
+                                        <p>Agência: <strong>0001</strong></p>
+                                        <p>Número da Conta: <strong> 35644330-6</strong></p>
+                                        <p>CPF: <strong>126.853.717-96</strong></p>
+                                        <p>Valor: <strong> {{ config('cart.currency_symbol') }} {{ $total }}</strong></p>
+                                        <p><strong><small class="text-danger text">* {{ config('bank-transfer.note') }}</small></strong></p>
+                                        <p><strong><small class="text-danger text">*Enviar o comprovante de depósito para o  número: (21) 99698-2844 - Sarita Marques</small></strong></p>
                                 </div>
                             </div>
                         </div>
