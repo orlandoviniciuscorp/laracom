e0813897b63e48ae55ede11a628ac948341bd00c
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2020-04-24 21:00:04 +0000

    Layout inicial

--- resources/views/layouts/front/app.blade.php
+++ resources/views/layouts/front/app.blade.php
@@ -63,7 +63,7 @@
     </p>
 </noscript>
 <section>
-    @if(env('SHOPPING_OPENED') == 1)
+    @if(env('SHOPPING_OPENED') == 1231)
     <div class="hidden-xs">
         <div class="container">
             <div class="clearfix"></div>
@@ -126,20 +126,25 @@
 
         <div class="row">
             <h1>
-                Pedidos de 3a até 5a feira. Entrega aos sábados.
+                Pedidos de 2a até 4a feira. Entrega aos sábados.
                 <br /> <br />
                 Agradecemos pela visita.
+                <br /><br />
+                Dúvidas entre em contato pelo Whatsapp:
+                <a href="https://api.whatsapp.com/send?phone=5521996982844&text=Ol%C3%A1!%20Estou%20entrando%20em%20contato%20para%20saber%20mais%20inforna%C3%A7%C3%B5es%20sobre%20as%20compras%20no%20Sitio%20Demedeiros">
+                    (21)99698-2844</a>
             </h1>
         </div>
 
         <div class="row">
             Siga nossas redes sociais:
             <ul class="footer-social text-success">
-                <li> <a href="https://www.facebook.com/OrganicosParaTodosAAT"> <i class="fa fa-facebook text-primary" aria-hidden="false"></i>  </a> </li>
+                <li> <a href="https://www.facebook.com/demedeiros.organico.3"> <i class="fa fa-facebook text-primary" aria-hidden="false"></i>  </a> </li>
                 {{--<li> <a href=""> <i class="fa fa-twitter" aria-hidden="true"></i>   </a> </li>--}}
-                <li> <a href="https://www.instagram.com/organicosparatodos/"> <i class="fa fa-instagram text-primary" aria-hidden="true"></i>  </a> </li>
+                <li> <a href="https://www.instagram.com/sitio.demedeiros"> <i class="fa fa-instagram text-primary" aria-hidden="true"></i>  </a> </li>
                 {{--<li> <a href=""> <i class="fa fa-pinterest-p" aria-hidden="true"></i>  </a> </li>--}}
             </ul>
+
         </div>
 
     </div>
