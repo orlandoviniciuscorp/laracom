1e69965c2f9cc582e432ce3925770d743d44cda0
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2020-04-26 15:07:00 +0000

    cherry-pick

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -10,29 +10,32 @@
 
 @section('content')
     @include('layouts.front.home-slider')
+    @if(env('SHOPPING_OPENED' == 213121))
+        @foreach($cats as $cat)
+            @if($cat->products->isNotEmpty() && $cat->status == 1)
+                <section class="new-product t100 home">
+                    <div class="container">
+                        <div class="section-title b50" id="{{$cat->slug}}">
 
-    @foreach($cats as $cat)
-        @if($cat->products->isNotEmpty() && $cat->status == 1)
-            <section class="new-product t100 home">
-                <div class="container">
-                    <div class="section-title b50" id="{{$cat->slug}}">
+                            <h2>{{ $cat->name }}</h2>
 
-                        <h2>{{ $cat->name }}</h2>
-
+                        </div>
+                        <p class="text-center">
+                            <a href="{{ route('cart.index') }}" title="View Cart" class="btn btn-success product-info">
+                                Ver carrinho de compras
+                            </a>
+                        </p>
+                        <br/>
+                        @include('front.products.product-list', ['products' => $cat->products->where('status', 1),
+                                                                 'category_slug'=>$cat->slug])
+                        <div id="browse-all-btn"> <a class="btn btn-default browse-all-btn" href="{{ route('front.category.slug', $cat->slug) }}" role="button">Ver todos os ítens do produtor</a></div>
                     </div>
-                    <p class="text-center">
-                        <a href="{{ route('cart.index') }}" title="View Cart" class="btn btn-success product-info">
-                            Ver carrinho de compras
-                        </a>
-                    </p>
-                    <br/>
-                    @include('front.products.product-list', ['products' => $cat->products->where('status', 1),
-                                                             'category_slug'=>$cat->slug])
-                    <div id="browse-all-btn"> <a class="btn btn-default browse-all-btn" href="{{ route('front.category.slug', $cat->slug) }}" role="button">Ver todos os ítens do produtor</a></div>
-                </div>
-            </section>
+                </section>
+            @endif
+            <hr>
+        @endforeach
+        @else
+            @include('front.closed')
         @endif
-        <hr>
-    @endforeach
     @include('mailchimp::mailchimp')
 @endsection
