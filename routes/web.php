a84cd531ae0721d80864732dcf85198142fef114
orlandovinicius1987 <orlando.vinicius1987@gmail.com>
2020-04-28 15:28:14 -0300

    Cancelamento de pedido

--- routes/web.php
+++ routes/web.php
@@ -114,6 +114,7 @@ Route::namespace('Front')->group(function () {
         });
 
         Route::get('accounts', 'AccountsController@index')->name('accounts');
+        Route::post('cancel-order', 'AccountsController@cancelOrder')->name('accounts.cancel-order');
         Route::get('checkout/{courier_id}', 'CheckoutController@index')->name('checkout.index');
         Route::post('checkout/store', 'CheckoutController@store')->name('checkout.store');
         Route::post('checkout/cart', 'CheckoutController@index')->name('cart.checkout');
