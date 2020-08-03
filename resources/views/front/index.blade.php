42d62259a5c4fe8b31e94b4fdba3eac87d3c2b15
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2020-08-03 21:58:33 +0000

    Acerto no Layout

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -4,7 +4,7 @@
     <meta property="og:type" content="home"/>
     <meta property="og:title" content="{{ config('app.name') }}"/>
     <meta property="og:description" content="{{ config('app.name') }}"/>
-    <meta property="og:image" itemprop="image" content="https://organicosaat.com.br/img/logo_feira.jpg">
+    <meta property="og:image" itemprop="image" content="https://organicosaat.com.br/img/logo_feira.png">
 
 @endsection
 
