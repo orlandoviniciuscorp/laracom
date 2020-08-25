e86e9c292dc476fa7b8fe84ee02157ea3f96d485
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2020-08-25 21:48:27 +0000

    Correções

--- app/Http/Controllers/Front/CategoryController.php
+++ app/Http/Controllers/Front/CategoryController.php
@@ -39,7 +39,7 @@ class CategoryController extends Controller
 
         return view('front.categories.category', [
             'category' => $category,
-            'products' => $repo->paginateArrayResults($products, 20),
+            'products' => $repo->paginateArrayResults($products, 100),
             'cats'=>$this->getCategoryOrder()
         ]);
     }
