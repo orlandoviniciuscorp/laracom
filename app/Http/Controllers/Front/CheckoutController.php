8abd20932daba69eba20dfd5811e8aefcd1e8e02
Orlando Vinicius <orlando.vinicius1987@gmail.com>
2021-06-26 20:23:39 -0300

    Retirando a sacola

--- app/Http/Controllers/Front/CheckoutController.php
+++ app/Http/Controllers/Front/CheckoutController.php
@@ -335,10 +335,14 @@ class CheckoutController extends Controller
 
     public function neededBag()
     {
+
+        if(current_shop() == 2){
+            return;
+        }
         $carItens = $this->cartRepo->getCartItemsTransformed();
         $hasBag = false;
         foreach($carItens as $carItem){
-            if($carItem->name == 'Sacola Retornável' || $carItem->name == 'Sacola Retornável (Rio)'){
+            if($carItem->name == 'Sacola Retornável'){
                 $hasBag = true;
 
             }
@@ -346,13 +350,10 @@ class CheckoutController extends Controller
 
         if ((!is_null(auth()->user())) && auth()->user()->countBought() < 1 && !$hasBag) {
 
-            if(current_shop() == 1) {
-                $product = $this->productRepo->findByProductName('Sacola Retornável');
-                $options = [];
-            }else{
-                $product = $this->productRepo->findByProductName('Sacola Retornável (Rio)');
-                $options = [];
-            }
+
+            $product = $this->productRepo->findByProductName('Sacola Retornável');
+            $options = [];
+
             $this->cartRepo->addToCart($product,1,$options);
         }
 
